8892002fb67e437355cd29d396a0aaa09c599582
“SohaibHegazy” <sohaib.mohamed@hotmail.com>
2024-05-03 15:18:27 -0700

    modify

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-""" fabric function to archive directory """
+'''
+a Fabric script that generates a .tgz archive from
+the contents of the web_static folder
+'''
 from datetime import datetime
 from fabric.api import local
 import os
@@ -8,11 +11,12 @@ import os
 def do_pack():
     try:
         if not os.path.exists("versions"):
-            local('mkdir versions')
-        t = datetime.now()
-        f = "%Y%m%d%H%M%S"
-        archive_path = 'versions/web_static_{}.tgz'.format(t.strftime(f))
-        local('tar -cvzf {} web_static'.format(archive_path))
-        return archive_path
-    except:
+            local("mkdir -p versions")
+
+        now = datetime.now()
+        arch_name = "web_static_{}.tgz".format(now.strftime("%Y%m%d%H%M%S"))
+        arch_path = "versions/{}".format(arch_name)
+        local("tar -cvzf {} web_static".format(arch_path))
+        return arch_path
+    except Exception:
         return None
