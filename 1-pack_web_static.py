92cf79826f16890c97fd41b72b8973ec64ed3742
“SohaibHegazy” <sohaib.mohamed@hotmail.com>
2024-05-03 15:29:25 -0700

    modify

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,6 +3,7 @@
 a Fabric script that generates a .tgz archive from
 the contents of the web_static folder
 '''
+
 from datetime import datetime
 from fabric.api import local
 import os
@@ -12,7 +13,6 @@ def do_pack():
     try:
         if not os.path.exists("versions"):
             local("mkdir -p versions")
-
         now = datetime.now()
         arch_name = "web_static_{}.tgz".format(now.strftime("%Y%m%d%H%M%S"))
         arch_path = "versions/{}".format(arch_name)
