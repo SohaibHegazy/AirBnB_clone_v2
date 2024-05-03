35a16ef5240a90ad590b1e661f7a3825f9b0e9e6
“SohaibHegazy” <sohaib.mohamed@hotmail.com>
2024-05-03 14:46:00 -0700

    modify

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,15 +9,14 @@ import os
 
 
 def do_pack():
-    if not os.path.exists("versions"):
-        os.makedirs("versions")
+    try: 
+        if not os.path.exists("versions"):
+            os.makedirs("versions")
 
-    now = datetime.now()
-    compress_name = "web_static_{}.tgz".format(now.strftime("%Y%m%d%H%M%S"))
-    compress_path = "versions/{}".format(compress_name)
-    c = local("tar -cvzf {} web_static".format(compress_path), capture=True)
+        now = datetime.now()
+        compress_name = "web_static_{}.tgz".format(now.strftime("%Y%m%d%H%M%S"))
+        compress_path = "versions/{}".format(compress_name)
+        local("tar -cvzf {} web_static".format(compress_path))
 
-    if c.succeeded:
-        return compress_path
-    else:
+    except:
         return None
