9c24e959fe6721504cd66004d41e3bbd32a4586f
“SohaibHegazy” <sohaib.mohamed@hotmail.com>
2024-05-03 15:15:21 -0700

    modify

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -11,12 +11,11 @@ import os
 def do_pack():
     try:
         if not os.path.exists("versions"):
-            local("mkdir -p versions")
-
-        now = datetime.now()
-        arch_name = "web_static_{}.tgz".format(now.strftime("%Y%m%d%H%M%S"))
-        arch_path = "versions/{}".format(arch_name)
-        local("tar -cvzf {} web_static".format(arch_path))
-        return arch_path
-    except Exception:
+            local('mkdir versions')
+        t = datetime.now()
+        f = "%Y%m%d%H%M%S"
+        archive_path = 'versions/web_static_{}.tgz'.format(t.strftime(f))
+        local('tar -cvzf {} web_static'.format(archive_path))
+        return archive_path
+    except:
         return None
